f1c6ba78eb71af9f5cabbd29f9ea7436a8f7f8d8
Vincent Wilms <git@m1.apollo3zehn.net>
2026-05-23 14:42:05 +0200

    Make case insensitive

--- src/Minimail/Core/MiniMailboxFilter.cs
+++ src/Minimail/Core/MiniMailboxFilter.cs
@@ -28,7 +28,7 @@ namespace Minimail
             try
             {
                 var toAddress = to.AsAddress();
-                var toAddressWithoutDomain = toAddress.Replace("@m1.apollo3zehn.net", "");
+                var toAddressWithoutDomain = toAddress.Replace("@m1.apollo3zehn.net", "").ToLower();
 
                 if (_whitelist.ContainsKey(toAddressWithoutDomain))
                 {
